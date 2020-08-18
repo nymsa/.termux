e3ee42a1285299c348d36935720dea0e0fa099d6
nymsa <27156495+nymsa@users.noreply.github.com>
2020-08-18 17:52:54 +0300

    Initial commit 1. imap-checker with termux-notification 2. termux job (15s) 3. termux properties

---
+++ imap-checker.py
@@ -0,0 +1,31 @@
+import os
+from imaplib import IMAP4_SSL
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("imap-checker.ini")
+imap_host = config['MailRu']['imap_host']
+imap_port = config['MailRu']['imap_port']
+username = config['MailRu']['username']
+token = config['MailRu']['token']
+action_url = config['MailRu']['action_url']
+history_url = config['MailRu']['history_url']
+
+with IMAP4_SSL(host=imap_host, port=imap_port) as M:
+    M.login(username, token)
+    M.select()
+    return_code, mail_ids = M.search(None, 'UnSeen')
+    mail_ids_splitted= mail_ids[0].decode().split(" ")
+    if mail_ids_splitted[0] == '':
+        unread_count = 0
+        os.system("termux-notification-remove mail_ru_checker")
+    else:
+        unread_count = len(mail_ids_splitted)
+        os.system("termux-notification -i imap-checker \
+        -t 'mail.ru' \
+        -c 'unread messages: {}' \
+        --action 'termux-open-url {}' \
+        --button1 'history' \
+        --button1-action 'termux-open-url {}' \
+        --led-color 0000FF".format(unread_count, action_url, history_url))
+
